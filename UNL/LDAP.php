aabaac43f278e7d22017eb2bdbb9424948bfc479
Brett Bieber <brett.bieber@gmail.com>
2008-01-30 18:50:25 +0000

    remove test. Add headers

--- UNL/LDAP.php
+++ UNL/LDAP.php
@@ -8,6 +8,14 @@
  * echo UNL_LDAP::getConnection()->getFirstAttribute('bbieber2', 'sn');
  * </code>
  *
+ * PHP version 5
+ * 
+ * @category  Default 
+ * @package   UNL_LDAP
+ * @author    Brett Bieber <brett.bieber@gmail.com>
+ * @copyright 2007 Regents of the University of Nebraska
+ * @license   http://www1.unl.edu/wdn/wiki/Software_License BSD License
+ * @link      http://pear.unl.edu/package/UNL_LDAP
  */
 class UNL_LDAP
 {
